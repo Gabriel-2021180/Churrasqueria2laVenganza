c4c901cce1080e74a94d2d932d04b6a9615fa341
MongoDB <81056601+Gabriel-2021180@users.noreply.github.com>
2023-09-08 02:04:33 -0400

    sexo al fin se pudo

--- Controllers/productosController.js
+++ Controllers/productosController.js
@@ -1,12 +1,17 @@
 const { Request, Response } = require('express');
 const Product = require('../Models/Productos');
+const { Storage } = require('@google-cloud/storage');
+const storage = new Storage({ keyFilename: "googleimage.json" });
+
 const moment = require('moment');
 // Crear un nuevo producto
 exports.createProduct = async (req, res) => {
   try {
-    const url = await uploadFile(req.file);
     const { nombre, descripcion, precio } = req.body;
-    const newProduct = await Product.create({ nombre, descripcion, precio,url});
+    console.log("estos son los datos",nombre,descripcion,precio,req.file)
+    const imagen = await uploadFile(req.file);
+    console.log("estos son los datos",nombre,descripcion,precio,imagen)
+    const newProduct = await Product.create({ nombre, descripcion, precio,imagen});
     console.log("los datos son"+newProduct)
     res.status(201).json({ message: 'Producto creado exitosamente', product: newProduct });
   } catch (error) {
