69c511bc492e6f74ce738cd764db838a6f1e3ad2
EnderHack93 <75341553+EnderHack93@users.noreply.github.com>
2023-09-12 11:37:56 -0400

    ojo 54

--- Router/Router.js
+++ Router/Router.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const storage = multer.memoryStorage();
-const { createProduct, getActiveProducts, getInactiveProducts, editProduct, deactivateProduct } = require('../Controllers/productosController');
+const { createProduct, getActiveProducts, getInactiveProducts, editProduct, deactivateProduct, getProduct } = require('../Controllers/productosController');
 const upload = multer({ storage }).single('imagencita');
 router.post('/crear', upload,createProduct);
 router.get('/activos', getActiveProducts);
-router.get('producto',)
+router.get('producto',getProduct)
 router.get('/inactivos', getInactiveProducts);
 router.put('/editar/:id', editProduct);
 router.put('/desactivar/:id', deactivateProduct);
